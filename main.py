c610fe4ff50ca9bc42183652ae0888df4a563115
Nastya <makarshinaa1@gmail.com>
2018-08-19 21:08:46 +0300

    Creating classes

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-print(123)
-a = 1
+class Shcool:
+    name = "МБОУ СОШ 15"
+    city = "Зеленодольск"
+
+    def set(self, name, city):
+        self.name = name
+        self.city = city
+
+
+gymnasium = Shcool()
+gymnasium.set("Gymnasium 1", "Zelenodolsk")
+print(gymnasium.name + ", " + gymnasium.city)
