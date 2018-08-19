fd4edf2f1d39554fef92d4118f3b947b5d617c8f
AnastasiaMakarshina <35972879+AnastasiaMakarshina@users.noreply.github.com>
2018-08-19 22:09:47 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+class Shcool:
+    name = "МБОУ СОШ 15"
+    city = "Зеленодольск"
+
+    def __init__(self, name, city):
+        self.name = name
+        self.city = city
+
+
+gymnasium = Shcool("Gymnasium 1", "Zelenodolsk")
+# gymnasium.set("Gymnasium 1", "Zelenodolsk")
+print(gymnasium.city)
+
+
+class University(Shcool):
+   # inclination = technical
+
+    def __init__(self, name, city, inclination):
+        Shcool.__init__(self, name, city)
+        self.inclination = inclination
+
+
+
+KFY = University("KFY", "Kazan", inclination="technical")
+print(KFY.inclination)
