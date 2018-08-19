f1dbe6d7d3007389b8b1a7749f378e8747bca70f
Nastya <makarshinaa1@gmail.com>
2018-08-19 22:02:02 +0300

    The inheritance method

--- main.py
+++ main.py
@@ -2,11 +2,23 @@ class Shcool:
     name = "МБОУ СОШ 15"
     city = "Зеленодольск"
 
-    def set(self, name, city):
+    def __init__(self, name, city):
         self.name = name
         self.city = city
 
 
-gymnasium = Shcool()
-gymnasium.set("Gymnasium 1", "Zelenodolsk")
-print(gymnasium.name + ", " + gymnasium.city)
+gymnasium = Shcool("Gymnasium 1", "Zelenodolsk")
+# gymnasium.set("Gymnasium 1", "Zelenodolsk")
+print(gymnasium.city)
+
+
+class University(Shcool):
+   # inclination = technical
+
+    def __init__(self, name, city, inclination):
+        Shcool.__init__(self, name, city)
+        self.inclination = inclination
+
+
+KFY = University("KFY", "Kazan", inclination="technical")
+print(KFY.inclination)
