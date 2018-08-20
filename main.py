7d40a66a803bcb60225c11942b46ed7aa581b838
Nastya <makarshinaa1@gmail.com>
2018-08-20 22:27:04 +0300

    Added polymorphism

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 class Shcool:
     name = "МБОУ СОШ 15"
     city = "Зеленодольск"
+    age = 200
 
-    def __init__(self, name, city):
+    def __init__(self, name, city, age):
         self.name = name
         self.city = city
+        self.age = age
+
+    def display_info(self):
+        print("Название:", self.name, ", ", "возраст:", self.age)
 
 
-gymnasium = Shcool("Gymnasium 1", "Zelenodolsk")
-# gymnasium.set("Gymnasium 1", "Zelenodolsk")
-print(gymnasium.city)
+# gymnasium = Shcool("Gymфasium 1", "Zelenodolsk", 122)
+# print(gymnasium.city)
 
 
 class University(Shcool):
-   # inclination = technical
 
-    def __init__(self, name, city, inclination):
-        Shcool.__init__(self, name, city)
+    def __init__(self, name, city, age, inclination):
+        Shcool.__init__(self, name, city, age)
         self.inclination = inclination
 
+    def display_info(self):
+        print("Университет", self.name, "город", self.city)
 
-KFY = University("KFY", "Kazan", inclination="technical")
+
+KFY = University("KFY", "Kazan", 111, inclination="technical")
 print(KFY.inclination)
+
+result = [Shcool("Школа 22", "Зеленодольск", 22), University("IT", "Казань", 22, inclination="humanitarian")]
+
+for shcool in result:
+    shcool.display_info()
+    print()
